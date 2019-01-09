369e58302cf2a9359aeb5ebea5f68b234ea06d04
Brent <brent@Brents-MacBook-Pro.local>
2019-01-09 16:58:12 -0500

    commit

---
+++ 03-challenges/challenge1.js
@@ -0,0 +1,15 @@
+let word1 = 'milk'; 
+
+console.log(word1);
+
+let word2 = word1.split("");
+
+console.log(word2);
+
+let word3 = word2.reverse();
+
+console.log(word3);
+
+let word4 = word3.join("");
+
+console.log(word4);
